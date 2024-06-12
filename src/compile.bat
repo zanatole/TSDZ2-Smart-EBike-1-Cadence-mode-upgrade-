0e553a59e1603362b1027080e46f49cf012721d0
emmebrusa <m.brusa@libero.it>
2024-06-12 17:29:03 +0200

    Update compile.bat

--- src/compile.bat
+++ src/compile.bat
@@ -1,11 +1,11 @@
 @echo off
 if exist "%windir%\syswow64\cmd.exe" goto CYGWIN64
 
-PATH = %PATH%;%~dp0..\tools\cygwin_32\bin;
+PATH = %PATH%;%~dp0..\tools\cygwin_32\bin;C:\SDCC\usr\local\bin;C:\SDCC\bin;C:\%PROGRAMFILES%\SDCC\usr\local\bin;C:\%PROGRAMFILES%\SDCC\bin;
 goto BUILD
 
 :CYGWIN64
-PATH = %PATH%;%~dp0..\tools\cygwin_64\bin;
+PATH = %PATH%;%~dp0..\tools\cygwin_64\bin;C:\SDCC\usr\local\bin;C:\SDCC\bin;C:\%PROGRAMFILES%\SDCC\usr\local\bin;C:\%PROGRAMFILES%\SDCC\bin;
 
 :BUILD
 echo Build started...
